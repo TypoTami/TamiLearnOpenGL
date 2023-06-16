3d5b54407fc39da4dfa439f0cffd660a1b4e942a
TypoTami <up2010614@myport.ac.uk>
2023-06-16 21:13:00 +0200

    Texture drawing with texture unit

--- shaders/tri.frag
+++ shaders/tri.frag
@@ -1,8 +1,13 @@
 #version 330 core
 out vec4 FragColor;
+
 in vec3 ourColour;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-    FragColor = vec4(ourColour, 1.0f);
+//    FragColor = vec4(ourColour, 1.0f);
+    FragColor = texture(ourTexture, TexCoord) * vec4(ourColour, 1.0f);
 }
--- shaders/tri.glsl
+++ shaders/tri.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;     // The posisition variable has attribute position 0
 layout (location = 1) in vec3 aColour;  // The colour varable has attribute position 1
+layout (location = 2) in vec2 aTexCoord;// The texture coordinates has attribute position 2
 
 out vec3 ourColour;
+out vec2 TexCoord;
 
 void main()
 {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
     ourColour = aColour;
+    TexCoord = aTexCoord;
 }
